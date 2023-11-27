5ca79e6cfc6d3d41942f1bfb9a3a46e26eeb1413
Ubuntu <ubuntu@ip-172-31-39-6.ap-south-1.compute.internal>
2023-11-27 20:31:55 +0000

    harsha commits this

---
+++ harsha.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from datetime import datetime,timedelta
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+
+def TEST():
+    a=1
+    b='hello'
+    c=a+b
+    return c
+default_args={
+    'owner': 'airflow',
+    'start_date':datetime(2023, 1, 15),
+    'email': ['harshavardhan.bale@transcendstreet.com'],
+    'email_on_failure': True,
+    'depends_on_past':False,
+    'retries':0
+}
+dag=DAG(dag_id='DAG-1',default_args=default_args,catchup=False,schedule_interval='@once')
+#hai
+hello_operator = PythonOperator(task_id='hello_task', python_callable=TEST, dag=dag)
+hello_operator
